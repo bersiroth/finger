fe475e4b0d417184d2a9cfaccbe1db5f14f06d26
bersiroth <caronbernard1@gmail.com>
2018-08-02 00:09:04 +0200

    test format table

--- index.js
+++ index.js
@@ -8,17 +8,48 @@ httpApi.post('/finger-new-player', handleNewPlayer);
 httpApi.listen(process.env.PORT);
 
 console.log('init server');
+
+let channels = [];
+
 async function handleNewPlayer(req, res) {
 
-	console.log('new player');
-	let result = 'bien recu !';
+	console.log('new player' + req.body.user_name);
+
+	let result;
 	try {
-		// await createEval(req.body.channel_id, req.body.user_id, req.body.text);
 		console.log(req.body.channel_id);
 		console.log(req.body.user_id);
+
+        channels[req.body.channel_id] = [];
+		channels[req.body.channel_id][req.body.user_id] = {
+        	user_id : req.body.channel_id,
+        	user_name : req.body.user_name,
+			point : 0
+		};
+
+		result = channelToString(req.body.channel_id);
 	} catch(e) {
 		result = e;
 	} finally {
 		res.status(200).send(result);
+		//console.log(result);
 	}
 }
+
+function channelToString(channel_id) {
+	let string = "| " + "id".padEnd(10) + " | " + "name".padEnd(10) + " | " + "point".padEnd(10) + " | \n";
+
+    channels[channel_id].forEach(function (channel) {
+        string += "| " + channel['user_id'].toString().padEnd(10) + " | " + channel['user_name'].padEnd(10) + " | " + channel['point'].toString().padEnd(10) + " | \n";
+	});
+
+    return string;
+}
+/*
+handleNewPlayer({
+    body : {
+        channel_id : 1,
+        user_id : '12',
+        user_name : 'bernard',
+	}
+}, null);*/
