ef88fcdb7a183af3daef8a1eab20237d97220224
bersiroth <caronbernard1@gmail.com>
2018-08-02 00:48:42 +0200

    test format table

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ async function handleNewPlayer(req, res) {
 
 function channelToString(channel_id) {
 	let string = "| " + "id".padEnd(10) + " | " + "name".padEnd(10) + " | " + "point".padEnd(10) + " | \n";
+	 string += "| " + "id".padEnd(10) + " | " + "name".padEnd(10) + " | " + "point".padEnd(10) + " | \n";
 
     channels[channel_id].forEach(function (channel) {
         string += "| " + channel.user_id.toString().padEnd(10) + " | " + channel.user_name.padEnd(10) + " | " + channel.point.toString().padEnd(10) + " | \n";
