a5a7bc7040325ac56484f24f0762715de049e441
bersiroth <caronbernard1@gmail.com>
2018-08-01 14:54:45 +0200

    test heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let httpApi = express();
 httpApi.use(bodyParser.json());
 httpApi.use(bodyParser.urlencoded({extended: true}));
 httpApi.post('/finger-new-player', handleNewPlayer);
-httpApi.listen(24605);
+httpApi.listen(5000);
 
 console.log('init server');
 async function handleNewPlayer(req, res) {
