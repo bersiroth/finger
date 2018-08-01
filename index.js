6168e1dbe233c8e7f1174cfd37520d11f1e0ce4b
bersiroth <caronbernard1@gmail.com>
2018-08-01 14:31:08 +0200

    test heroku

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let httpApi = express();
+httpApi.use(bodyParser.json());
+httpApi.use(bodyParser.urlencoded({extended: true}));
+httpApi.post('/finger-new-player', handleNewPlayer);
+httpApi.listen(24605);
+
+console.log('init server');
+async function handleNewPlayer(req, res) {
+
+	console.log('new player');
+	let result = '';
+	try {
+		// await createEval(req.body.channel_id, req.body.user_id, req.body.text);
+		console.log(req.body.channel_id);
+		console.log(req.body.user_id);
+	} catch(e) {
+		result = e;
+	} finally {
+		res.status(200).send(result);
+	}
+}
